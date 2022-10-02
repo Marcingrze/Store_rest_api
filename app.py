519e30268bf09afdf99f98703833316f590b1fda
Marcin Grześkowiak <79422088+Marcingrze@users.noreply.github.com>
2022-10-02 20:16:29 +0200

    change endpoints

--- app.py
+++ app.py
@@ -1,18 +1,20 @@
 from flask import Flask, request
 from db import stores, items
+import uuid
 
 app = Flask(__name__)
 
 
 @app.get("/store")
 def get_stores():
-    return {"stores": stores}
+    return {"stores": list(stores.values())}
 
 @app.post("/store")
 def create_store():
-    request_data = request.get_json()
-    new_store = {"name": request_data["name"], "items": []}
-    stores.append(new_store)
+    store_data = request.get_json()
+    store_id = uuid.uuid4().hex
+    new_store = {**store_data, "id": store_id}
+    stores[store_id] = new_store
     return new_store, 201
 
 @app.post("/store/<string:name>/item")
@@ -32,9 +34,9 @@ def get_item_in_store(name: str):
             return {"items": store["items"]}, 201
     return {"message": "store not found"}, 404
 
-@app.get("/store/<string:name>")
-def get_store_info(name : str):
-    for store in stores:
-        if store["name"] == name:
-            return {"store": store}, 201
-    return {"message": "store not found"}, 404
+@app.get("/store/<string:store_id>")
+def get_store_info(store_id : str):
+    try:
+        return stores[store_id], 201
+    except KeyError:
+        return {"message": "store not found"}, 404
