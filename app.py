1f9ace1a060469fec170829fd3c1b118dd485551
Marcin Grześkowiak <79422088+Marcingrze@users.noreply.github.com>
2022-10-02 20:32:01 +0200

    change endpoints

--- app.py
+++ app.py
@@ -9,33 +9,42 @@ app = Flask(__name__)
 def get_stores():
     return {"stores": list(stores.values())}
 
+
 @app.post("/store")
 def create_store():
     store_data = request.get_json()
     store_id = uuid.uuid4().hex
-    new_store = {**store_data, "id": store_id}
-    stores[store_id] = new_store
-    return new_store, 201
+    store = {**store_data, "id": store_id}
+    stores[store_id] = store
+    return store, 201
 
-@app.post("/store/<string:name>/item")
-def add_item(name: str):
-    request_data = request.get_json()
-    for store in stores:
-        if store["name"] == name:
-            new_item = {"name": request_data["name"], "price": request_data["price"]}
-            store["items"].append(new_item)
-            return new_item, 201
-    return {"message": "store not found"}, 404
 
-@app.get("/store/<string:name>/item")
-def get_item_in_store(name: str):
-    for store in stores:
-        if store["name"] == name:
-            return {"items": store["items"]}, 201
-    return {"message": "store not found"}, 404
+@app.post("/item")
+def create_item():
+    item_data = request.get_json()
+    if item_data["store_id"] not in stores:
+        return {"message": "Store not found"}, 404
+
+    item_id = uuid.uuid4().hex
+    item = ({**item_data, "id": item_id})
+    return item, 201
+
+
+@app.get("/item")
+def get_all_items():
+    return {"items": list(items.values())}
+
+
+@app.get("/item/<string:item_id>")
+def get_item_(item_id: str):
+    try:
+        return items[item_id], 201
+    except KeyError:
+        return {"message": "item not found"}, 404
+
 
 @app.get("/store/<string:store_id>")
-def get_store_info(store_id : str):
+def get_store_info(store_id: str):
     try:
         return stores[store_id], 201
     except KeyError:
